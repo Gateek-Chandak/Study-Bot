0dc3a42bd2544683bd358c58a6e9cca01eda65a8
Gateek <chandakgateek@gmail.com>
2024-08-12 19:12:34 -0400

    initialized openai query optimizer

--- server/index.js
+++ server/index.js
@@ -1,10 +1,21 @@
+// IMPORTS
 const express = require('express')
+const cors = require('cors');
 
-const router = express()
+const app = express()
 
+// ROUTES
 const openApiRoutes = require('./routes/openApiRoutes')
 
+// GLOBAL CONSTANTS
 const PORT = 4000
+
+// MIDDLEWARE
+app.use(cors({
+    origin: 'http://localhost:3000', //frontend origin
+    methods: 'GET,POST', // allowed methods
+    allowedHeaders: 'Content-Type,Authorization', //allowed headers
+}));
 
 app.use(express.json())
 
@@ -13,8 +24,10 @@ app.use((req, res, next) => {
     next()
 })
 
+// API ROUTES
 app.use("/api/openai", openApiRoutes)
 
+// INITIALIZATION
 app.listen(PORT, () => {
     console.log(`Connected to port ${PORT}`)
 })
--- server/routes/openApiRoutes.js
+++ server/routes/openApiRoutes.js
@@ -1,6 +1,37 @@
+// IMPORTS
 const express = require('express');
+const Configuration = require('openai');
+const OpenAIApi = require('openai')
 require('dotenv').config();
 
+// ROUTER INIT
 const router = express.Router();
+
+// CONSTANTS
+const OPEN_AI_API_KEY = process.env.OPEN_AI_API_KEY
+
+// OPEN AI CONFIG
+const configuration = new Configuration({
+    apiKey: OPEN_AI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+// OPTIMIZE USER QUERY
+router.post('/query-optimizer', async (req, res) => {
+    const { prompt } = req.body
+
+    try {
+        const completion = await openai.chat.completions.create({
+            messages: [{ role: "user", content: `give me ONE youtube prompt that would give me the best information about ${prompt}. Do not add any extra words, just return the prompt` }],
+            model: "gpt-4o",
+        });
+        const response = await completion.choices[0].message.content
+
+        res.status(200).json({ response });
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ error: "Error generating response" });
+    }
+});
 
 module.exports = router;
